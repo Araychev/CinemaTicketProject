2a365f7c17e39dcb3f5a83ed299c5f5da4305aa2
Angel Raychev <angelraychev@gmail.com>
2022-03-21 20:14:27 +0200

    Create Database

---
+++ CinemaTicketWeb/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CinemaTicketWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [StringLength(100, MinimumLength = 1,ErrorMessage = "{0} must be between {2} and {1} characters!")]
+        public string Name { get; set; }
+
+        [Range(1,100, ErrorMessage = "{0} must be between {1} and {2}")]
+        [Display(Name = "Display Order")]
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
