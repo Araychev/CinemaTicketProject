7174c6561332e5d9eec018eefd482e50697e4c82
Angel Raychev <angelraychev@gmail.com>
2022-03-23 20:34:35 +0200

    Genre View models created

---
+++ CinemaTicketWeb/Areas/Admin/Controllers/GenreController.cs
@@ -0,0 +1,123 @@
+﻿using CinemaTicket.Core.Contracts;
+using CinemaTicket.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaTicketWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class GenreController : Controller
+    {
+        private readonly IUnitOfWork _db;
+
+        public GenreController(IUnitOfWork db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Genre> ganre = _db.Genre.GetAll();
+            return View(ganre);
+        }
+        
+        //Get
+        public IActionResult Create()
+        {
+            
+            return View();
+        } 
+        
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Genre obj)
+        {
+            if (_db.Genre.GetAll().Any(x=>x.Name == obj.Name))
+            {
+                ModelState.AddModelError("name", "This genre name exist!");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Genre.Add(obj);
+                _db.Save();
+
+                TempData["success"] = "Genre created successfully";
+
+                    return RedirectToAction("Index");
+            }
+
+
+            return View(obj);
+        }
+
+        //Get
+        public IActionResult Edit(Guid? id)
+        {
+            if (id==null)
+            {
+                return NotFound();
+            }
+            var genreFromDb = _db.Genre.GetFirstOrDefault(u=>u.Id==id);;
+            if (genreFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(genreFromDb);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Genre obj)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                _db.Genre.Update(obj);
+                _db.Save();
+
+                TempData["success"] = "Genre updated successfully";
+                return RedirectToAction("Index");
+            }
+
+
+            return View(obj);
+        }
+
+        //Get
+        public IActionResult Delete(Guid? id)
+        {
+            
+           
+            if (id == null )
+            {
+                return NotFound();
+            }
+
+            var genreFromDb = _db.Genre.GetFirstOrDefault(u=>u.Id==id);
+            if (genreFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(genreFromDb);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(Guid? id)
+        {
+
+
+            var obj = _db.Genre.GetFirstOrDefault(u=>u.Id==id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+                _db.Genre.Remove(obj);
+                _db.Save();
+                TempData["success"] = "Genre deleted successfully";
+                return RedirectToAction("Index");
+            
+        }
+    }
+}
