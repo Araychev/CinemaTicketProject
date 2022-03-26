dd845bddeaa36995fc4b905bbcf5b9b0a613863d
Angel Raychev <angelraychev@gmail.com>
2022-03-26 15:39:30 +0200

    Home Controller edited and views  added

--- CinemaTicketWeb/Areas/Customer/Controllers/HomeController.cs
+++ CinemaTicketWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using CinemaTicket.Core.Contracts;
 using CinemaTicket.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,21 +8,39 @@ namespace CinemaTicketWeb.Areas.Customer.Controllers
     [Area("Customer")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        
+        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _db;
+
+        public HomeController(ILogger<HomeController> logger,IUnitOfWork db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Ticket> tickets = _db.Ticket.GetAll(includeProperties:"Category,Genre");
+            return View(tickets);
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+
+
+        public IActionResult Details(int ticketId)
+        {
+            ShoppingCart cartObj = new()
+            {
+                Count = 1,
+                TicketId = ticketId,
+                Ticket = _db.Ticket.GetFirstOrDefault(t => t.Id == ticketId, includeProperties: "Category,Genre"),
+
+            };
+            return View(cartObj);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
