e6525bd77b8d6033e172b05c31da7045f737fc51
Angel Raychev <angelraychev@gmail.com>
2022-03-27 14:43:49 +0300

    Register form upgrade

--- CinemaTicketWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CinemaTicketWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -119,7 +119,14 @@ namespace CinemaTicketWeb.Areas.Identity.Pages.Account
 
         public async Task OnGetAsync(string returnUrl = null)
         {
-            
+            //if (_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
+            //{
+            //    _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+            //    _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+            //    _roleManager.CreateAsync(new IdentityRole(SD.Role_User_Comp)).GetAwaiter().GetResult();
+            //    _roleManager.CreateAsync(new IdentityRole(SD.Role_User_Indi)).GetAwaiter().GetResult();   
+            //}
+           
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             Input = new InputModel()
